ac7a5dd90b4e638b73f89ca0d2b71d7d3bf86ca9
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-01-08 19:58:33 +0530

    Added Welcome to Hashtable and BinarySearchTree

---
+++ DataStructureDay15/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStructureDay15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hashtable and BinarySearchTree of Data structure");
+        }
+    }
+}
