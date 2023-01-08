44a09df8c12528e7880a0bdc0649551a6d486d09
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-01-08 20:20:56 +0530

    Modified to Find Frequency Of Words program

--- DataStructureDay15/Program.cs
+++ DataStructureDay15/Program.cs
@@ -5,6 +5,36 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hashtable and BinarySearchTree of Data structure");
+            Console.WriteLine("Enter the option given below");      //Taking user input
+            Console.WriteLine("1:Find frequency of words");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            static void CountNumbOfOccurence(string paragraph)          //Method to get final frequency
+            {
+                MyMapNode<string, int> hashTabe = new MyMapNode<string, int>(6);
+
+                string[] words = paragraph.Split(' ');
+
+                foreach (string word in words)
+                {
+                    if (hashTabe.Exists(word.ToLower()))
+                        hashTabe.Add(word.ToLower(), hashTabe.Get(word.ToLower()) + 1);
+                    else
+                        hashTabe.Add(word.ToLower(), 1);
+                }
+                Console.WriteLine("Displaying after add operation");
+                hashTabe.Display();
+            }
+            switch (option)
+            {
+                case 1:
+                    CountNumbOfOccurence("To be or not to be");
+                    break;
+                default:
+                    Console.WriteLine("Please choose correct option");
+                    break;
+
+            }
         }
     }
 }
